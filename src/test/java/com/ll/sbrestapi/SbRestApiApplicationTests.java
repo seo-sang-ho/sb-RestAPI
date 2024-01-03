aabf467fec82a79b3fe5ab2fbec076a5e5ddf3a3
seo-sang-ho <rocter2049@naver.com>
2024-01-03 10:44:04 +0900

    feat: add application dev,test

--- src/test/java/com/ll/sbrestapi/SbRestApiApplicationTests.java
+++ src/test/java/com/ll/sbrestapi/SbRestApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.sbrestapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SbRestApiApplicationTests {
 
     @Test
