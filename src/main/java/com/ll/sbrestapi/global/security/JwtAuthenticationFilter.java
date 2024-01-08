eebfb13d120d8943318272a47f46986242f1d1d5
seo-sang-ho <rocter2049@naver.com>
2024-01-09 03:53:41 +0900

    feat: find user to username in parameter

--- src/main/java/com/ll/sbrestapi/global/security/JwtAuthenticationFilter.java
+++ src/main/java/com/ll/sbrestapi/global/security/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.ll.sbrestapi.global.security;
 
+import com.ll.sbrestapi.domain.member.member.entity.Member;
+import com.ll.sbrestapi.domain.member.member.service.MemberService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,20 +19,42 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final MemberService memberService;
+
     @Override
     @SneakyThrows
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
 
-        User user = new User("user1","", List.of());
+        String username = request.getParameter("username");
+
+        if (username != null) {
+
+            Member member = memberService.findByUsername(username).get();
+
+            User user = new User(
+                    member.getUsername(),
+                    member.getPassword(),
+                    List.of()
+            );
+
+            Authentication auth = new UsernamePasswordAuthenticationToken(
+                    user,
+                    user.getPassword(),
+                    user.getAuthorities()
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        User user = new User("user1","",List.of());
 
         Authentication auth = new UsernamePasswordAuthenticationToken(
                 user,
                 user.getPassword(),
                 user.getAuthorities()
         );
-
-        SecurityContextHolder.getContext().setAuthentication(auth);
-
-        filterChain.doFilter(request, response);
     }
 }
