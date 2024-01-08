e087eab8cb1994deae03c19706871195575545d6
seo-sang-ho <rocter2049@naver.com>
2024-01-08 22:41:37 +0900

    feat: check login:

--- src/main/java/com/ll/sbrestapi/domain/member/article/article/controller/ApiV1ArticlesController.java
+++ src/main/java/com/ll/sbrestapi/domain/member/article/article/controller/ApiV1ArticlesController.java
@@ -11,8 +11,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/articles")
@@ -115,8 +117,14 @@ public class ApiV1ArticlesController {
     }
 
     @PostMapping("")
-    public RsData<?> writeArticle(@RequestBody WriteArticleRequestBody body){
+    public RsData<?> writeArticle(@RequestBody WriteArticleRequestBody body, Principal principal){
         Member member = rq.getMember();
+
+        Optional.ofNullable(principal)
+                .ifPresentOrElse(
+                        p -> System.out.println("로그인 :" + p.getName()),
+                        () -> System.out.println("비로그인"));
+
         RsData<Article> writeRs = articleService.write(member, body.getTitle(), body.getBody());
 
         return writeRs.of(
